94554c34359262da1cba6c0c40ca085ae557a214
David Haas <davidhaas6@gmail.com>
2024-11-19 12:47:00 -0500

    initial simulator

---
+++ app/api/chat/route.ts
@@ -0,0 +1,43 @@
+import { supabase } from '@/app/lib/initSupabase';
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+
+const world_sim_system = `You are a sophisticated world simulation engine powered by text. Each message in the conversation represents a step in the simulation. The user may update the simulation through events and time changes.
+Each step, provide a **concise** 40-50 word update of the universe's state. The progress should reflect the change in time between steps and any events, demonstrating the fractal-like complexity of evolution within a universe. 
+Provide your updates in a highly condensed but comprehensive form, maximizing the amount of information conveyed. Also provide the relevant **state** and other parameters of the simulation in a JSON object. This information should contextualize the simulation update.
+
+# Example output:
+This is an information-dense update for the simulation.
+{
+    \"relevant parameter\": \"value\",
+    \"some data\": [123, 243],
+    \"a more complex parameter\": {...}
+}`
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: openai('gpt-4o-mini'),
+    system: world_sim_system,
+    messages,
+  });
+
+
+  const { data, error } = await supabase
+  .from('simulation_events')
+  .insert([
+    {simulation_id: 1232, update: "test"},
+  ])
+  .select()
+  console.log(error)
+  console.log(data)
+        
+
+  return result.toDataStreamResponse();
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,139 @@
+// page.tsx
+'use client';
+
+import { Message, useChat, useCompletion } from 'ai/react';
+import next from 'next';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+
+function processMessage(message: Message) {
+  if (message.role === 'user') {
+    return null;
+  } else {
+    const completion = message.content;
+    const bracket_idx = completion.indexOf('{');
+    let text_update = completion;
+    let state = {}
+
+    if (bracket_idx >= 0) {
+      text_update = completion.substring(0, bracket_idx)
+      if (completion.split('{').length == completion.split('}').length) {  // heuristic for json completeness
+        let json_update = completion.substring(bracket_idx)
+        try {
+          state = JSON.parse(json_update.replaceAll('\n', ''))
+        }
+        catch (SyntaxError) {
+          console.error("Could not parse generated json: ", json_update)
+        }
+      }
+    }
+    return { text_update, state };
+  }
+}
+
+function useSimulation(simEvent?: string, timeDelta = 1) {
+  const [simTime, setTime] = useState(0);
+  const { messages, append } = useChat({ onResponse: () => { } });
+  const nextStep = useCallback(
+    () => {
+      const newTime = simTime + timeDelta;
+      setTime(newTime);
+      let prompt = "TIME: " + simTime
+      if (simEvent) {
+        prompt += '\nEvent: ' + simEvent
+      }
+      console.log(prompt)
+
+      append({ role: "user", content: prompt });
+    },
+    [simTime, timeDelta, simEvent],
+  )
+
+  return { messages, nextStep };
+}
+
+
+function displayState(worldState: any) {
+  let fields = [];
+  for (const key in worldState) {
+    if (worldState.hasOwnProperty(key)) {
+      fields.push(
+        <li><b>{key}</b>: {worldState[`${key}`].toString()}</li>
+      )
+    }
+  }
+  return <ul>{fields}</ul>
+}
+
+
+export default function Page() {
+  const [eventField, setEventField] = useState<string>('');
+  const eventContainerRef = useRef<HTMLDivElement>(null);
+  const { messages, nextStep } = useSimulation(eventField);
+
+  const handleFormInput = (event: any) => {
+    setEventField(event.target.value);
+  };
+
+  const nextStepButton = (event: any) => {
+    event.preventDefault(); // Prevent the form from refreshing the page
+    nextStep()
+    setEventField('')
+  };
+
+  const textStates = []
+  const jsonStates = []
+  for (let message of messages) {
+    const processed = processMessage(message);
+    if (processed) {
+      let { text_update, state } = processed;
+      textStates.push(text_update);
+      jsonStates.push(state);
+    }
+  }
+
+  
+
+  // Function to scroll to the bottom
+  const scrollToBottom = () => {
+    if (eventContainerRef.current) {
+      eventContainerRef.current.scrollTop = eventContainerRef.current.scrollHeight;
+    }
+  };
+
+  // Scroll to the bottom whenever `textStates` updates
+  useEffect(() => {
+    scrollToBottom();
+  }, [textStates]);
+
+  return (
+    <div className="main-container">
+      <div className="events-container panel" ref={eventContainerRef}>
+        <h1>Simulator</h1>
+        {textStates.map((text, i) => <p key={i} className='simulationStep'>{text} </p>)}
+      </div>
+
+      <div className="control-container">
+        
+        <div className="form-container panel">
+          <form onSubmit={nextStepButton} className="entry-form">
+            <div className='input-forms'>
+              <input
+                name="prompt"
+                value={eventField}
+                onChange={handleFormInput}
+                className="form-input"
+                placeholder=''
+              />
+            </div>
+            <br />
+            <button type="submit" className="next-button">Run</button>
+          </form>
+        </div>
+        <div className="state-panel panel">
+          {displayState(jsonStates.pop())}
+        </div>
+      </div>
+    </div>
+  );
+}
