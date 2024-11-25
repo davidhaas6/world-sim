aeb70890f4a511ef93f03fe725b76b79f9a44866
David Haas <davidhaas6@gmail.com>
2024-11-24 23:32:14 -0500

    graph styling

--- app/InfoPanel.tsx
+++ app/InfoPanel.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
-import { Line, LineChart, RadialBar, RadialBarChart, ReferenceLine, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
-import { retroOutrunPalette } from "./lib/palettes";
+import { Bar, BarChart, Line, LineChart, RadialBar, RadialBarChart, ReferenceLine, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { retroOutrunPalette, tab20cPalette, tab20Palette } from "./lib/palettes";
 
 type State = { [key in string]: number }
 type StateSeries = { [key in string]: number[] };
@@ -12,7 +12,7 @@ interface InfoPanelProps {
 
 const InfoPanel: FunctionComponent<InfoPanelProps> = (props: InfoPanelProps) => {
   const { stateData } = props;
-  if (stateData.length == 0) {
+  if (stateData.length == 0 || stateData == undefined) {
     return <div className="state-panel panel">
     </div>
   }
@@ -22,6 +22,7 @@ const InfoPanel: FunctionComponent<InfoPanelProps> = (props: InfoPanelProps) => 
   // form metric -> measurement series 
   for (const stateInstance of stateData) {
     for (const measure in stateInstance) {
+      // if (measure == 'time')
       if (metrics.hasOwnProperty(measure)) {
         metrics[measure].push(stateInstance[measure])
       }
@@ -33,7 +34,6 @@ const InfoPanel: FunctionComponent<InfoPanelProps> = (props: InfoPanelProps) => 
 
   const smolMetrics = [];
   const bigMetrics = [];
-  const radialMetrics = [];
   var metricColors: StringMap = {};
 
   let idx = 0;
@@ -46,51 +46,53 @@ const InfoPanel: FunctionComponent<InfoPanelProps> = (props: InfoPanelProps) => 
       smolMetrics.push(metric)
     }
 
-    metricColors[metric] = retroOutrunPalette[idx % retroOutrunPalette.length];
+    metricColors[metric] = tab20Palette[idx % retroOutrunPalette.length];
     idx += 1;
   }
 
+  const smolMetricGraph = (
+    <ResponsiveContainer width="100%" height={150}>
+      <LineChart data={stateData.slice()} dataKey={"time"}>
+        {smolMetrics.map((metric) => (
+          <Line
+            key={metric}
+            type="natural"
+            dataKey={metric}
+            stroke={metricColors[metric]}
+            strokeWidth={2}
+            dot={false}
+          />
+        ))}
+        <Tooltip />
+        {/* <XAxis height={15}  dataKey={"time"}/> */}
+      </LineChart>
+    </ResponsiveContainer>
+  );
 
-  console.log("bigmetrics", bigMetrics)
-  console.log("small metrics ", smolMetrics)
-  console.log("metric colors ", metricColors)
+  const bigMetricGraph = (
+    <ResponsiveContainer width="100%" height={150}>
+      <LineChart data={stateData} dataKey={"time"}>
+        {bigMetrics.map((metric) => (
+          <Line
+            key={metric}
+            type="natural"
+            dataKey={metric}
+            stroke={metricColors[metric]}
+            strokeWidth={2}
+            dot={false}
+          />
+        ))}
+        <Tooltip />
+        {/* <XAxis height={15} /> */}
+      </LineChart>
+    </ResponsiveContainer>
+  );
+
 
   return (
     <div className="state-panel panel">
-      <ResponsiveContainer width={200} height="100%">
-        <LineChart data={stateData} dataKey={"time"}>
-          {bigMetrics.map((metric) => (
-            <Line
-              key={metric}
-              type="natural"
-              dataKey={metric}
-              stroke={metricColors[metric]}
-              strokeWidth={2}
-              dot={false}
-            />
-          ))}
-          <Tooltip />
-          <XAxis height={15} />
-        </LineChart>
-      </ResponsiveContainer>
-      <ResponsiveContainer width={200} height="100%">
-        <LineChart data={stateData.slice()} dataKey={"time"}>
-          {smolMetrics.map((metric, index) => (
-            <Line
-              key={metric}
-              type="natural"
-              dataKey={metric}
-              stroke={metricColors[metric]}
-              strokeWidth={2}
-              dot={false}
-            />
-          ))}
-          <Tooltip />
-          {/* <YAxis width={20} allowDecimals={false} /> */}
-          <XAxis height={15} />
-        </LineChart>
-      </ResponsiveContainer>
-
+      {smolMetrics.length > 0 && smolMetricGraph}
+      {bigMetrics.length > 0 && bigMetricGraph}
     </div>
   );
 }
--- app/lib/palettes.ts
+++ app/lib/palettes.ts
@@ -19,3 +19,14 @@ export const retroOutrunPalette = [
   "#632848",
   "#6a6a2a",
 ];
+
+export const tab20cPalette = ["#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2", "#31a354", "#74c476", "#a1d99b", "#c7e9c0", "#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb", "#636363", "#969696", "#bdbdbd", "#d9d9d9"];
+
+export const tab20bPalette = ["#393b79", "#5254a3", "#6b6ecf", "#9c9ede", "#637939", "#8ca252", "#b5cf6b", "#cedb9c", "#8c6d31", "#bd9e39", "#e7ba52", "#e7cb94", "#843c39", "#ad494a", "#d6616b", "#e7969c", "#7b4173", "#a55194", "#ce6dbd", "#de9ed6"]
+
+export const tab20Palette = [
+  "#d62728", "#2ca02c", "#1f77b4", "#ff7f0e", "#e377c2",
+  "#9467bd", "#17becf", "#8c564b", "#bcbd22", "#aec7e8",
+  "#ffbb78", "#f7b6d2", "#98df8a", "#c5b0d5", "#c49c94",
+  "#9edae5", "#dbdb8d", "#ff9896", "#7f7f7f", "#c7c7c7"
+]
