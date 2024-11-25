07642e19139fb0fea37cc34a502ebf194598daba
David Haas <davidhaas6@gmail.com>
2024-11-24 22:07:37 -0500

    Multiple charts

--- app/InfoPanel.tsx
+++ app/InfoPanel.tsx
@@ -1,26 +1,14 @@
 import { FunctionComponent } from "react";
-import { Line, LineChart, ReferenceLine, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, RadialBar, RadialBarChart, ReferenceLine, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { retroOutrunPalette } from "./lib/palettes";
 
 type State = { [key in string]: number }
 type StateSeries = { [key in string]: number[] };
+type StringMap = { [key in string]: string }
 
 interface InfoPanelProps {
   stateData: State[]
 }
-
-
-function displayState(worldState: any) {
-  let fields = [];
-  for (const key in worldState) {
-    if (worldState.hasOwnProperty(key)) {
-      fields.push(
-        <li key={key}><b>{key}</b>: {worldState[key].toString()}</li>
-      )
-    }
-  }
-  return <ul>{fields}</ul>
-}
-
 
 const InfoPanel: FunctionComponent<InfoPanelProps> = (props: InfoPanelProps) => {
   const { stateData } = props;
@@ -28,7 +16,10 @@ const InfoPanel: FunctionComponent<InfoPanelProps> = (props: InfoPanelProps) => 
     return <div className="state-panel panel">
     </div>
   }
+
   var metrics: StateSeries = {}
+
+  // form metric -> measurement series 
   for (const stateInstance of stateData) {
     for (const measure in stateInstance) {
       if (metrics.hasOwnProperty(measure)) {
@@ -38,50 +29,68 @@ const InfoPanel: FunctionComponent<InfoPanelProps> = (props: InfoPanelProps) => 
         metrics[measure] = [stateInstance[measure]]
       }
     }
-
   }
 
-  const retroOutrunColors = [
-    "#6a2a6a",
-    "#2a6a6a",
-    "#684a70",
-    "#725863",
-    "#6d475a",
-    "#552565",
-    "#556525",
-    "#325c58",
-    "#5e3a1e",
-    "#663e41",
-    "#204060",
-    "#5a1c29",
-    "#452161",
-    "#266657",
-    "#2a5b21",
-    "#3a1e5e",
-    "#632848",
-    "#6a6a2a",
-  ];
+  const smolMetrics = [];
+  const bigMetrics = [];
+  const radialMetrics = [];
+  var metricColors: StringMap = {};
+
+  let idx = 0;
+  for (const metric in metrics) {
+    const measurements = metrics[metric];
+    if (Math.max(...measurements) > 100 || Math.min(...measurements) < -100) {
+      bigMetrics.push(metric)
+    }
+    else {
+      smolMetrics.push(metric)
+    }
+
+    metricColors[metric] = retroOutrunPalette[idx % retroOutrunPalette.length];
+    idx += 1;
+  }
+
+
+  console.log("bigmetrics", bigMetrics)
+  console.log("small metrics ", smolMetrics)
+  console.log("metric colors ", metricColors)
 
   return (
     <div className="state-panel panel">
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart height={300} data={stateData} dataKey={"time"}>
-          <ReferenceLine y={100} label="100" stroke="green" strokeDasharray="3 3" />
-          {Object.keys(metrics).map((metric, index) => (
+      <ResponsiveContainer width={200} height="100%">
+        <LineChart data={stateData} dataKey={"time"}>
+          {bigMetrics.map((metric) => (
             <Line
               key={metric}
               type="natural"
               dataKey={metric}
-              stroke={retroOutrunColors[index % retroOutrunColors.length]}
+              stroke={metricColors[metric]}
               strokeWidth={2}
               dot={false}
             />
           ))}
           <Tooltip />
-          <YAxis width={20} allowDecimals={false} />
-          <XAxis height={15} /> 
+          <XAxis height={15} />
         </LineChart>
       </ResponsiveContainer>
+      <ResponsiveContainer width={200} height="100%">
+        <LineChart data={stateData.slice()} dataKey={"time"}>
+          {smolMetrics.map((metric, index) => (
+            <Line
+              key={metric}
+              type="natural"
+              dataKey={metric}
+              stroke={metricColors[metric]}
+              strokeWidth={2}
+              dot={false}
+            />
+          ))}
+          <Tooltip />
+          {/* <YAxis width={20} allowDecimals={false} /> */}
+          <XAxis height={15} />
+        </LineChart>
+      </ResponsiveContainer>
+
     </div>
   );
 }
